ef01fe220d39bbb052fc4c178123cc598dd8a069
Breider Lopez <yessidlopez2018@gmail.com>
2023-07-03 18:01:37 -0500

    Previous pro version

--- src/Tarea_4_1/Main.java
+++ src/Tarea_4_1/Main.java
@@ -1,24 +1,28 @@
 package Tarea_4_1;
 
 import javax.swing.*;
+import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
-
-        String p = JOptionPane.showInputDialog(null, "ingrese el primer nombre y apellido de tu familiar".trim());
-        String d = JOptionPane.showInputDialog(null, "ingrese el segundo nombre y apellido de tu familiar ".trim());
-        String s = JOptionPane.showInputDialog(null, "ingrese el tercer nombre y apellido de tu familiar ".trim());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ingrese el primer nombre y apellido de tu familiar");
+        String p = scanner.nextLine();
+        System.out.println("ingrese el primer nombre y apellido de tu familiar");
+        String d = scanner.nextLine();
+        System.out.println("ingrese el primer nombre y apellido de tu familiar");
+        String s = scanner.nextLine();
         int tamano1 = p.length();
         int tamano2 = d.length();
         int tamano3 = s.length();
         if (tamano1 > tamano2 && tamano1 > tamano3) {
-            JOptionPane.showMessageDialog(null, "El dato " + p + "es el dato de mayor longitud teniendo " + tamano1 + " datos");
+            System.out.println("El dato " + p.toUpperCase() + " es el dato de mayor longitud teniendo " + tamano1 + " datos");
         } else if (tamano2 > tamano1 && tamano2 > tamano3) {
-            JOptionPane.showMessageDialog(null, "El dato " + d + "ingresado es el dato de mayor longitud teniendo " + tamano2 + " datos");
+            System.out.println("El dato " + d.toUpperCase() + " ingresado es el dato de mayor longitud teniendo " + tamano2 + " datos");
         } else if (tamano3 > tamano1 && tamano3 > tamano2) {
-            JOptionPane.showMessageDialog(null, "El dato " + s + " ingresado es el dato de mayor longitud teniendo " + tamano3 + " datos");
+            System.out.println("El dato " + s.toUpperCase() + " ingresado es el dato de mayor longitud teniendo " + tamano3 + " datos");
         } else {
             JOptionPane.showMessageDialog(null, "Hay mas de un dato con la misma longitud");
         }
