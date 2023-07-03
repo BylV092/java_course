de4a7b103d7b1655b4439ffca965775298035901
Breider Lopez <yessidlopez2018@gmail.com>
2023-07-03 17:25:16 -0500

    Previous pro version

--- src/Tarea_4_1/Main.java
+++ src/Tarea_4_1/Main.java
@@ -7,18 +7,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String p = JOptionPane.showInputDialog(null, "ingrese la primera palabra".trim());
-        String d = JOptionPane.showInputDialog(null, "ingrese la segunda palabra".trim());
-        String s = JOptionPane.showInputDialog(null, "ingrese la tercera palabra".trim());
+        String p = JOptionPane.showInputDialog(null, "ingrese el primer nombre y apellido de tu familiar".trim());
+        String d = JOptionPane.showInputDialog(null, "ingrese el segundo nombre y apellido de tu familiar ".trim());
+        String s = JOptionPane.showInputDialog(null, "ingrese el tercer nombre y apellido de tu familiar ".trim());
         int tamano1 = p.length();
         int tamano2 = d.length();
         int tamano3 = s.length();
         if (tamano1 > tamano2 && tamano1 > tamano3) {
-            JOptionPane.showMessageDialog(null, "El primer dato ingresado es el dato de mayor longitud teniendo " + tamano1 + "datos");
+            JOptionPane.showMessageDialog(null, "El dato " + p + "es el dato de mayor longitud teniendo " + tamano1 + " datos");
         } else if (tamano2 > tamano1 && tamano2 > tamano3) {
-            JOptionPane.showMessageDialog(null, "El segundo dato ingresado es el dato de mayor longitud teniendo " + tamano2 + "datos");
+            JOptionPane.showMessageDialog(null, "El dato " + d + "ingresado es el dato de mayor longitud teniendo " + tamano2 + " datos");
         } else if (tamano3 > tamano1 && tamano3 > tamano2) {
-            JOptionPane.showMessageDialog(null, "El tercer dato ingresado es el dato de mayor longitud teniendo " + tamano3 + "datos");
+            JOptionPane.showMessageDialog(null, "El dato " + s + " ingresado es el dato de mayor longitud teniendo " + tamano3 + " datos");
         } else {
             JOptionPane.showMessageDialog(null, "Hay mas de un dato con la misma longitud");
         }
