7ed1f8b447aaec1f5ee35fd757240bb3ddc84cfa
Breider Lopez <yessidlopez2018@gmail.com>
2023-07-23 20:46:08 -0500

    Tarea del modulo de hoy basado en hilos y su ejecucion y tipos corrigiendo la ejecucion en simultaneo de los thread

--- src/alfanumeric/Main.java
+++ src/alfanumeric/Main.java
@@ -1,24 +1,18 @@
 package alfanumeric;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Thread threadNumbers = new Thread(new Alfanumeric(Type.Number));
         Thread threadLetters = new Thread(new Alfanumeric(Type.Letter));
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the Thread to run: " +
-                "\n1 for Numbers Thread." +
-                "\n2 for Letters Thread.");
-        int d = scanner.nextInt();
-        if (d  ==1 || d==2){
-            if (d == 1) {
-                threadNumbers.start();
-            } else if (d == 2) {
-                threadLetters.start();
-            }
-        }else {
-            System.err.println("ERROr: //Option enter invalid");
+        threadNumbers.start();
+        try {
+            threadNumbers.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        threadLetters.start();
     }
+
+
 }
+
